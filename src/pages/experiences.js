cc50f5c04de3b31595651f8f3e760360b95c8b94
Yousef <jbygoal@gmail.com>
2019-01-15 23:59:18 +0300

    fix experiences

--- src/pages/experiences.js
+++ src/pages/experiences.js
@@ -1,23 +1,52 @@
 import React from "react";
-// import styled from "styled-components";
+import styled from "styled-components";
 
 import Carousel from "../components/Carousel/Carousel";
 import CarouselItem from "../components/Carousel/CarouselItem";
 
 export default class Experiences extends React.Component {
   render() {
+    const screenWidth = window.innerWidth;
     return (
       <section id="experiences">
         <h3>Experiences</h3>
-        <div>
+        {screenWidth < 700 ? (
           <Carousel>
             <CarouselItem index={1} />
             <CarouselItem index={2} />
             <CarouselItem index={3} />
             <CarouselItem index={4} />
           </Carousel>
-        </div>
+        ) : (
+          <Wrapper>
+            <Item />
+            <Item />
+            <Item />
+            <Item />
+          </Wrapper>
+        )}
       </section>
     );
   }
 }
+
+const Wrapper = styled.div`
+  width: 100%;
+  padding: 2rem;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+const Item = styled.div`
+  background: ${props => props.theme.secondary};
+  text-align: center;
+  width: 25%;
+  padding-bottom: 25%;
+  height: 0;
+  color: ${props => props.theme.secondary};
+  box-shadow: 0 0 1rem rgba(0, 0, 0, 0.1);
+  border-radius: 1rem;
+  margin: 2rem;
+`;
